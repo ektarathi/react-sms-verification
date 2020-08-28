b84d5a2c28009774719517875422244d04406dc6
ektarathi <rathiekta2801@gmail.com>
2020-08-28 16:36:12 +0100

    Added resend code functionality

--- src/components/Confirmation.tsx
+++ src/components/Confirmation.tsx
@@ -4,35 +4,33 @@ import { useNavigate } from "@reach/router";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
-import Link from '@material-ui/core/Link';
+import Link from "@material-ui/core/Link";
 import firebase from "../firebaseConfig";
+import Grid from "@material-ui/core/Grid";
 
 const useStyles = makeStyles((theme: Theme) =>
 	createStyles({
 		submitButton: {
 			width: "200px",
-        },
-        infoText: {
-            margin: 'auto',
-            textAlign: 'left'
-        },
-        textInput: {
-            width: '100%',
-            margin: '8px 8px 20px 1px'
-        },
-        resendLink: {
-            width: '100%',
-            marginTop: theme.spacing(2)
-        }
+		},
+		infoText: {
+			margin: "auto",
+			textAlign: "left",
+		},
+		textInput: {
+			width: "100%",
+			margin: "8px 8px 20px 1px",
+		},
 	})
 );
 export interface ConfirmationProps {
-    output: any;
-    number: string;
+	output: any;
+	number: string;
 }
 
 const Confirmation: React.SFC<ConfirmationProps> = ({
-	output, number
+	output,
+	number,
 }: ConfirmationProps) => {
 	const classes = useStyles();
 	const navigate = useNavigate();
@@ -57,77 +55,76 @@ const Confirmation: React.SFC<ConfirmationProps> = ({
 					setError(true);
 					setErrorMessage(error.message);
 					console.log(error.message);
-
 				});
 		} else {
 			setError(true);
-			setErrorMessage("Please enter a 6 digit OTP code.")
+			setErrorMessage("Please enter a 6 digit OTP code.");
 		}
 	};
 
-    const resendOTP = () => {
-		console.log('enter');
-        var applicationVerifier = new firebase.auth.RecaptchaVerifier(
-			'recaptcha-container');
-		var provider = new firebase.auth.PhoneAuthProvider();
-		provider.verifyPhoneNumber('+16505550101', applicationVerifier)
-			.then(function(verificationId) {
-			  var code = window.prompt('Please enter the verification ' +
-				  'code that was sent to your mobile device.');
-			  return firebase.auth.PhoneAuthProvider.credential(verificationId,
-				  code as any);
+	const resendOTP = () => {
+		var appVerifier = new firebase.auth.RecaptchaVerifier(
+			"recaptcha-container",
+			{
+				size: "invisible",
+				callback: function (response: any) {
+					resendOTP();
+				},
+			}
+		);
+		firebase
+			.auth()
+			.signInWithPhoneNumber(number, appVerifier)
+			.then((confirmationResult: any) => {
+				output = confirmationResult;
+				handleVerifyCode();
 			})
-			.then(function(phoneCredential) {
-			  return firebase.auth().signInWithCredential(phoneCredential);
+			.catch((error) => {
+				//alert(error.message);
+				console.log(error.message);
 			});
-        /*provider.verifyPhoneNumber(number, applicationVerifier)
-            .then(function(verificationId) {
-				console.log('Id is: ',verificationId);
-              var verificationCode = 'Please enter the verification ' +
-                  'code that was sent to your mobile device.';
-              return firebase.auth.PhoneAuthProvider.credential(verificationId,
-                  verificationCode);
-            })
-            .then(function(phoneCredential) {
-              return firebase.auth().signInWithCredential(phoneCredential);
-            });*/
-    }
+	};
 
 	return (
-		<div style={{margin: 'auto'}}>
-            <Typography variant="body2" gutterBottom className={classes.infoText}>
-                We have sent the security code to {number}.
-                <br/>
-                Enter it below to verify your phone number.
-            </Typography>
+		<div style={{ margin: "auto" }}>
+			<Typography variant="body2" gutterBottom className={classes.infoText}>
+				We have sent the security code to {number}.
+				<br />
+				Enter it below to verify your phone number.
+			</Typography>
 			<TextField
 				placeholder="Enter the verification code..."
 				value={verificationCode}
 				onChange={setCode}
 				label="Verification Code"
-                inputProps={{ maxLength: 6 }}
+				inputProps={{ maxLength: 6 }}
 				className={classes.textInput}
 				id="verification-code"
 				error={error ? true : false} //this will show error message only when there is error
-                helperText={errorMessage}
+				helperText={errorMessage}
 			/>
-            <div id="recaptcha-container"></div>
-			<Button
-				onClick={handleVerifyCode}
-				variant="contained"
-				color="primary"
-				className={classes.submitButton}
-			>
-				Submit Value
-			</Button>
-            <Link
-                component="button"
-                variant="body2"
-                className={classes.resendLink}
-                onClick={resendOTP}
-                >
-                Resend Code
-            </Link>
+			<div id="recaptcha-container"></div>
+			<Grid container spacing={3}>
+				<Grid item xs={12} sm={6}>
+					<Button
+						onClick={handleVerifyCode}
+						variant="contained"
+						color="primary"
+						className={classes.submitButton}
+					>
+						Submit Value
+					</Button>
+				</Grid>
+				<Grid item xs={12} sm={6}>
+					<Button
+						variant="contained"
+						className={classes.submitButton}
+						onClick={resendOTP}
+					>
+						Resend Code
+					</Button>
+				</Grid>
+			</Grid>
 		</div>
 	);
 };
